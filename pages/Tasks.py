f8d65299e07659eb1080aeb87c9b3dbfd9420401
pigpig1524 <vantuankiet.work@gmail.com>
2024-08-30 11:18:46 +0700

    update db client

--- pages/Tasks.py
+++ pages/Tasks.py
@@ -177,7 +177,13 @@ def get_json_content(file_path: str) -> dict:
  
 # MongoDB setup
 # client = pymongo.MongoClient("mongodb+srv://root:123@cluster0.ss6bc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", server_api=ServerApi('1'))
-client = pymongo.MongoClient("mongodb+srv://root:123@cluster0.ss6bc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+@st.cache_resource
+def init_connection():
+    return pymongo.MongoClient("mongodb+srv://root:123@cluster0.ss6bc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+
+client = init_connection()
+
+# client = pymongo.MongoClient("mongodb+srv://root:123@cluster0.ss6bc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 db = client['database']
 collection = db['new_info_DN']
  
